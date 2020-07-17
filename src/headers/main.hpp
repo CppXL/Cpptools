2b4a747277fdc9c9a885b9859c0e6010adeee4fb
CppXL <shijieanhaocqy@163.com>
2020-07-18 04:12:45 +0800

    func.cpp需要的头文件

---
+++ src/headers/main.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include<getopt.h>
+#include<getopt.h>
+#include<stdio.h>
+#include<cstdlib>
+#include<iostream>
+#include<bitset>
+#include<string>
+#include<iomanip>
+
+// 定义进制的十进制表达
+#define HEX 16
+#define OCT 8
+#define BIN 2
+#define DEC 10
+
+#define SS_CODE
+#define NUMSIZE 32
